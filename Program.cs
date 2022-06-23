124a3515ca3f5f195d898afc2a8f6a431bd395d5
JaraMohnson <jaramohnson@gmail.com>
2022-06-23 16:56:53 -0400

    updated Program.cs file to include a second loop

--- Program.cs
+++ Program.cs
@@ -1,35 +1,43 @@
 ﻿static int GetCubed(int x)
 {
-    return (x*x*x);
+    return (x * x * x);
 }
 
 static int GetSquare(int y)
 {
-    return(y*y);    
+    return (y * y);
 }
 
 Console.WriteLine("Learn your squares and cubes!");
 
 while (true)
 {
-    Console.WriteLine("Please enter a positive integer:");
-    int userNum = int.Parse(Console.ReadLine());
+    bool isDataValid = false;
+    int userNum = 0;
 
-    if (userNum <= 0)
+    while (isDataValid == false)
     {
-        Console.WriteLine("Please enter a valid number! The program will now restart!");
-        continue;
+        Console.WriteLine("Please enter a positive integer:");
+        userNum = int.Parse(Console.ReadLine());
+
+        if (userNum <= 0)
+        {
+            Console.WriteLine("Please enter a valid number!");
+        }
+
+        else if (userNum > 1290)
+        {
+            Console.WriteLine("Sorry, this number is too large, please choose a number less than or equal to 1,290.");
+        }
+        else
+        {
+            isDataValid = true;
+        }
     }
 
-    if (userNum > 1290)
-    {
-        Console.WriteLine("Sorry, this number is too large, please choose a number less than or equal to 1,290.");
-        continue;
-    }
-
-    Console.WriteLine(String.Format("{0,15} {1,15} {2,15}", "Number" , "Squared" , "Cubed"));
+    Console.WriteLine(String.Format("{0,15} {1,15} {2,15}", "Number", "Squared", "Cubed"));
     Console.WriteLine(String.Format("{0,15} {1,15} {2,15}", "======", "=======", "====="));
-  
+
     for (int i = 1; i <= userNum; i++)
     {
         Console.WriteLine(String.Format("{0,15} {1,15} {2,15}", i, GetSquare(i), GetCubed(i)));
